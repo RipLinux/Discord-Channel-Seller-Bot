f4b064738be0376f95b3fb4a96e8f331e8f5690b
Anas <anassayedaliali19_sd25@eng.bsu.edu.eg>
2025-05-01 05:22:56 +0300

    Add files via upload

---
+++ Models/Channels.js
@@ -0,0 +1,17 @@
+module.exports = {
+Channels:{
+    CategoryId:"",
+    Time1:"",
+    Time2:"",
+    Time3:"",
+    Time4:"",
+    Time5:""
+},
+Price:{
+    Time1:"",
+    Time2:"",
+    Time3:"",
+    Time4:"",
+    Time5:""
+}
+}
---
+++ Models/GuildData.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Guild:{
+        GuildId:"",
+        Probot:"282859044593598464",
+        Owner:""
+    },
+    Channels:{
+        Log:"",
+        CoolDown:"1m"
+    }
+}
